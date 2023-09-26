ed41bdd6a48d08f49c44f3ca1653b22e2f7e2ab3
jishan1019 <imranislamjishan200@gmail.com>
2023-09-27 01:36:18 +0600

    splash screen add

---
+++ app/src/main/java/com/dhakadevcraft/globallanguagetranslator/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.dhakadevcraft.globallanguagetranslator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final long SPLASH_DELAY = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        }, SPLASH_DELAY);
+    }
+}
