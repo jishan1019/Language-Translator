473d9d09a52145890b13baefbc572ef57aca6de9
jishan1019 <imranislamjishan200@gmail.com>
2024-04-09 21:18:35 +0600

    update apps

--- app/src/main/java/com/dhakadevcraft/globallanguagetranslator/StringConstants.java
+++ app/src/main/java/com/dhakadevcraft/globallanguagetranslator/StringConstants.java
@@ -5,9 +5,9 @@ public class StringConstants {
 //    private static final String FB_BANNER_ID = "IMG_16_9_APP_INSTALL#YOUR_PLACEMENT_ID";
 //    private static final String FB_INTERSTITIAL_ID = "IMG_16_9_APP_INSTALL#YOUR_PLACEMENT_ID";
 
-    private static final String FB_BANNER_ID = "932730361817497_932733988483801";
-    private static final String FB_INTERSTITIAL_ID = "932730361817497_932734205150446";
-    private static final String PRIVACY_URL = "https://softitblog.xyz/translatorAppPrivacy";
+    private static final String FB_BANNER_ID = "932730361817497_1006213177802548";
+    private static final String FB_INTERSTITIAL_ID = "932730361817497_1006213531135846";
+    private static final String PRIVACY_URL = "https://appslabbd.buzz/translatorAppPrivacy/";
 
     public static String getFbBannerId() {
         return FB_BANNER_ID;
